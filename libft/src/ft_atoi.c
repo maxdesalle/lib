9552a3b4546d34599a6aab6c4807ccd90e807e3d
maxdesalle <maxdesalle@pm.me>
2020-11-20 17:48:01 +0100

    quick modification of ft_atoi to handle overflows

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 11:34:55 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/18 14:45:31 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/20 17:47:15 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	result;
-	int	sign;
+	int				sign;
+	long long int	result;
 
 	result = 0;
 	sign = 1;
@@ -24,6 +24,7 @@ int	ft_atoi(const char *nptr)
 	if (*nptr == '+' || *nptr == '-')
 		*nptr++ == '-' && (sign = -1);
 	while (*nptr >= '0' && *nptr <= '9')
-		result = result * 10 + *nptr++ - '0';
+		if ((ret = ret * 10 + *nptr++ - '0') < 0)
+			return (~sign ? -1 : 0);
 	return (sign * result);
 }
