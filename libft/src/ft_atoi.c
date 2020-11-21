69307de4c0b08810c61812fabc1562b5e498e68a
maxdesalle <maxdesalle@pm.me>
2020-11-21 10:43:24 +0100

    quick correction (again) of ft_atoi

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 11:34:55 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/20 18:04:26 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/21 10:43:03 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ int	ft_atoi(const char *nptr)
 		*nptr++ == '-' && (sign = -1);
 	while (*nptr >= '0' && *nptr <= '9')
 		if ((result = result * 10 + *nptr++ - '0') < 0)
-			(~sign ? -1 : 0);
+			return (~sign ? -1 : 0);
 	return (sign * result);
 }
