16aef13de8ccee783ac72e4c7098a401dda5275f
maxdesalle <maxdesalle@pm.me>
2020-11-21 12:25:20 +0100

    correction of ft_memmove

--- libft/src/ft_memmove.c
+++ libft/src/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 13:48:35 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/20 17:27:21 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/21 12:25:08 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*s1;
 	const unsigned char	*s2;
 
-	if (dest == src)
-		return (dest);
 	s1 = dest;
 	s2 = src;
-	if (s1 < s2)
+	if (dest == src)
+		return (dest);
+	if (s2 < s1)
 	{
 		s1 += n;
 		s2 += n;
