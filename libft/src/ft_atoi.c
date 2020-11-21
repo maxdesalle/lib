a376adc7adfbe28f8887d6dcffbfacc567fd3350
maxdesalle <maxdesalle@pm.me>
2020-11-21 15:31:16 +0100

    correction of ft_atoi

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 11:34:55 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/21 10:43:03 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/21 15:31:06 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int				sign;
-	long long int	result;
+	int	sign;
+	int	result;
 
-	result = 0;
 	sign = 1;
-	while (*nptr <= 32)
+	result = 0;
+	while (*nptr && ((*nptr >= 9 && *nptr <= 13) || *nptr == ' '))
 		nptr++;
-	if (*nptr == '+' || *nptr == '-')
-		*nptr++ == '-' && (sign = -1);
-	while (*nptr >= '0' && *nptr <= '9')
-		if ((result = result * 10 + *nptr++ - '0') < 0)
-			return (~sign ? -1 : 0);
-	return (sign * result);
+	if (*nptr == '-')
+		sign = -1;
+	if (*nptr == '-' || *nptr == '+')
+		nptr++;
+	while (*nptr && *nptr >= '0' && *nptr <= '9')
+		result = result * 10 + (*nptr++ - '0');
+	return (result * sign);
 }
