29d89f40acb837376cfd7a40071c2769e59b4a58
maxdesalle <maxdesalle@pm.me>
2020-11-21 12:36:11 +0100

    fixed ft_strncmp by adding protection

--- libft/src/ft_strncmp.c
+++ libft/src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 14:55:42 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/16 16:39:17 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/21 12:35:43 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < (n - 1))
+	if (n == 0)
+		return (0);
+	while (s1[i] && s1[i] == s2[i] && i < (n - 1))
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
