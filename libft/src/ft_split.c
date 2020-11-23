3e73a522d9d1c0ff7ccdee1c4c5dc86f2364c5a1
maxdesalle <maxdesalle@pm.me>
2020-11-23 10:34:09 +0100

    finally a working ft_split!

--- libft/src/ft_split.c
+++ libft/src/ft_split.c
@@ -6,42 +6,89 @@
 /*   By: mdesalle <mdesalle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 09:21:49 by mdesalle          #+#    #+#             */
-/*   Updated: 2020/11/20 11:15:35 by mdesalle         ###   ########.fr       */
+/*   Updated: 2020/11/23 10:33:43 by mdesalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Credit goes to Oom (https://github.com/oomsveta) for this one. */
-
-static size_t	word_counter(const char *s, char c)
+static int		string_number(char const *s1, char c)
 {
-	size_t len;
+	int	i;
+	int	j;
 
-	len = 1;
-	while (*s)
-		if (*s++ == c)
-			len++;
+	i = 0;
+	j = 0;
+	if (!(*s1))
+		return (0);
+	while (*s1)
+	{
+		if (*s1 == c)
+			j = 0;
+		else if (j == 0)
+		{
+			j = 1;
+			i++;
+		}
+		s1++;
+	}
+	return (i);
+}
+
+static int		modified_strlen(char const *s2, char c, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s2[i] && s2[i] != c)
+	{
+		len++;
+		i++;
+	}
 	return (len);
+}
+
+static char		**clear_split(char const **dest, int j)
+{
+	while (j--)
+		free((void *)dest[j]);
+	free(dest);
+	return (NULL);
+}
+
+static char		**create_split(char const *s, char **dest, char c, int l)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	while (s[i] && j < l)
+	{
+		k = 0;
+		while (s[i] == c)
+			i++;
+		if (!(dest[j] = malloc(sizeof(char) * (modified_strlen(s, c, i) + 1))))
+			return (clear_split((char const **)dest, j));
+		while (s[i] && s[i] != c)
+			dest[j][k++] = s[i++];
+		dest[j][k] = '\0';
+		j++;
+	}
+	dest[j] = 0;
+	return (dest);
 }
 
 char			**ft_split(char const *s, char c)
 {
-	char	**splittedstring;
-	size_t	len;
-	int		i;
-	int		j;
+	char	**dest;
+	int		l;
 
-	len = word_counter((const char *)s, c);
-	if (!(splittedstring = malloc(sizeof(char *) * (len + 1))))
+	if (s == NULL)
 		return (NULL);
-	splittedstring[len] = '\0';
-	i = ft_strlen(s);
-	while ((j = i--) >= 0)
-	{
-		while (i >= 0 && s[i] != c)
-			i--;
-		splittedstring[--len] = ft_substr(s, i + 1, j - i - 1);
-	}
-	return (splittedstring);
+	l = string_number(s, c);
+	if (!(dest = (char **)malloc(sizeof(char *) * (l + 1))))
+		return (NULL);
+	return (create_split(s, dest, c, l));
 }
